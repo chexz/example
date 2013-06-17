040e23cb5d96661771472316ba731253f3045171
chexiangzhi <chexiangzhi@gmail.com>
2013-06-16 23:37:18 -0700

    improvement. readme, restructure
    add read me. divide main function into separate functions

--- src/ck/EntryApp.java
+++ src/ck/EntryApp.java
@@ -2,55 +2,102 @@ package ck;
 
 import java.io.*;
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.Vector;
 
 import jxl.*;
 import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
 
-public class EntryApp implements UncaughtExceptionHandler {// an application
-	static public void printUsage() {
-		System.out.println("Usage : java -jar ElementClassify.jar elementTable.xls result.xls source01.xls ...");
-		System.exit(-255);
+public class EntryApp implements UncaughtExceptionHandler {
+	String elementTableFilename = "elementTable.xls";
+	String resultFilename = "result.xls";
+	Vector<String> sourceFilenames = new Vector<String>();
+	
+	public String getElementTableFilename() {
+		return elementTableFilename;
+	}
+	
+	public String getResultFilename() {
+		return resultFilename;
+	}
+	
+	public Vector<String> getSourceFilenames() {
+		return sourceFilenames;
 	}
 
-	public static void main(String args[]) {
-
-		if (args.length <= 3) {
+	public void printUsage() {
+		System.out.println("Usage : java -jar ElementClassify.jar [--element-table elementTable.xls] [--result result.xls] source01.xls source02.xls ...");
+		System.exit(-255);
+	}
+	
+	private boolean isValidExcelFilename(String filename) {
+		if (filename.toLowerCase().endsWith(".xls")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void parseArgument(String args[]) {
+		if (args == null) {
+			printUsage();
+			return;
+		}
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("--element-table") && i < args.length - 1) {
+				++i;
+				if (!isValidExcelFilename(args[i])) {
+					System.err.println("Element table filename is not valid : " + args[i]);
+					System.exit(-4);
+				}
+				elementTableFilename = args[i];
+				System.out.println("Element table file name is " + elementTableFilename);
+				continue;
+			}
+			if (args[i].equalsIgnoreCase("--result") && i < args.length - 1) {
+				++i;
+				if (!isValidExcelFilename(args[i])) {
+					System.err.println("Result filename is not valid : " + args[i]);
+					System.out.println("Use result.xls as result filename");
+				} else {
+					resultFilename = args[i];
+				}
+				System.out.println("Result file name is " + resultFilename);
+				continue;
+			}
+			if (!isValidExcelFilename(args[i])) {
+				System.err.println("Ignore invalid source filename : " + args[i]);
+			} else {
+				sourceFilenames.add(args[i]);
+			}
+		}
+		if (sourceFilenames.isEmpty()) {
+			System.out.println("no source files");
 			printUsage();
 		}
+	}
+	
+	ElementProperty[] elements;
 
-		System.out.println(System.getProperty("user.dir"));
-
-		// save input file name
-		String[] fileName = new String[args.length];
-
-		for (int i = 0; i < args.length; i++) {
-			// System.out.println(args[i]);
-			fileName[i] = args[i];
-			System.out.println(fileName[i]);
-		}
-
+	public void readElementTable(String filename) {
 		// open elementTableFile and save element to ElementTable array
 		Workbook elementBook = null;
 		try {
-			elementBook = Workbook.getWorkbook(new File(fileName[0]));
+			elementBook = Workbook.getWorkbook(new File(filename));
 		} catch (Exception e) {
-			System.out.println(e);
+			e.printStackTrace();
 		}
 
 		if (elementBook == null) {
-			System.out.println("Get workbook failed!");
-			return;
+			System.err.println("open element table file failed!");
+			System.exit(-5);
 		}
 
 		Sheet sheet = elementBook.getSheet(0); // get the fist sheet
 
 		int rowNum = sheet.getRows();
-		// System.out.println(rowNum);
 
 		int columnNum = sheet.getColumns();
-		// System.out.println(columnNum);
 
 		ElementProperty[] element = new ElementProperty[rowNum - 1];
 
@@ -92,19 +139,95 @@ public class EntryApp implements UncaughtExceptionHandler {// an application
 		}
 
 		elementBook.close();
+		
+		elements = element;
+	}
+	
+	public boolean parseSourceFiles() {
+		return true;
+	}
 
+	public static void main(String args[]) {
+		System.out.println(System.getProperty("user.dir"));
+		EntryApp app = new EntryApp();
+		app.parseArgument(args);
+
+		// 1. read element table
+		//app.readElementTable(app.getElementTableFilename());
+		// open elementTableFile and save element to ElementTable array
+		Workbook elementBook = null;
+		try {
+			elementBook = Workbook.getWorkbook(new File(app.getElementTableFilename()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (elementBook == null) {
+			System.err.println("open element table file failed!");
+			System.exit(-5);
+		}
+
+		Sheet sheet = elementBook.getSheet(0); // get the fist sheet
+
+		int rowNum = sheet.getRows();
+
+		int columnNum = sheet.getColumns();
+
+		ElementProperty[] element = new ElementProperty[rowNum - 1];
+
+		int xMaxColumn = -1;
+		int xMinColumn = -1;
+		int yMaxColumn = -1;
+		int yMinColumn = -1;
+
+		for (int i = 1; i < columnNum; i++) {
+			String cellCnt = sheet.getCell(i, 0).getContents();
+			// System.out.println(cellCnt);
+			if (cellCnt.compareToIgnoreCase("xMax") == 0) {
+				xMaxColumn = i;
+			} else if (cellCnt.compareToIgnoreCase("xMin") == 0) {
+				xMinColumn = i;
+			} else if (cellCnt.compareToIgnoreCase("yMax") == 0) {
+				yMaxColumn = i;
+			} else if (cellCnt.compareToIgnoreCase("yMin") == 0) {
+				yMinColumn = i;
+			}
+
+			// System.out.println("xMax="+xMaxColumn+"xMin="+xMinColumn+"yMax="+yMaxColumn+"yMin="+yMinColumn);
+		}
+
+		for (int i = 1; i < rowNum; i++) {
+			int index = i - 1;
+			element[index] = new ElementProperty();
+			element[index].elementName = sheet.getCell(0, i).getContents();
+			element[index].xMax = Double.parseDouble(sheet.getCell(xMaxColumn,
+					i).getContents());
+			element[index].xMin = Double.parseDouble(sheet.getCell(xMinColumn,
+					i).getContents());
+			element[index].yMax = Double.parseDouble(sheet.getCell(yMaxColumn,
+					i).getContents());
+			element[index].yMin = Double.parseDouble(sheet.getCell(yMinColumn,
+					i).getContents());
+			// System.out.println("element["+index+"].elementName="+element[index].elementName+"element["+index+"].xMax="+element[index].xMax);
+			// System.out.println("element["+index+"].yMin="+element[index].yMin+"element["+index+"].yMax="+element[index].yMax);
+		}
+
+		elementBook.close();
+		
 		// open sample file, read all data and record all data
-		String[] sampleName = new String[fileName.length - 2];
+		Vector<String> sources = app.getSourceFilenames();
+		String[] sampleName = new String[sources.size()];
 		String[][] value = new String[element.length][sampleName.length];
 
-		for (int i = 2; i < fileName.length; i++) {
-			System.out.println("filename = " + fileName[i]);
-			String tempS[] = fileName[i].split("\\.");
-			sampleName[i - 2] = tempS[0];
+		for (int i = 0; i < sources.size(); i++) {
+			String filename = sources.get(i);
+			System.out.println("source filename = " + filename);
+			String tempS[] = filename.split("\\.");
+			sampleName[i] = tempS[0];
 			// System.out.println("tempS[0]="+tempS[0]+"  sampleName["+i+"]="+sampleName[i-1]);
 
 			try {
-				Workbook book = Workbook.getWorkbook(new File(fileName[i]));
+				Workbook book = Workbook.getWorkbook(new File(filename));
 				sheet = book.getSheet(0);
 				rowNum = sheet.getRows();
 				columnNum = sheet.getColumns();
@@ -123,8 +246,11 @@ public class EntryApp implements UncaughtExceptionHandler {// an application
 					} else if (cellCnt.compareToIgnoreCase("value") == 0) {
 						valueColumn = j;
 					}
-
 					// System.out.println("x="+xColumn+"y="+yColumn+"value="+valueColumn);
+				}
+				if (xColumn == -1 || yColumn == -1 || valueColumn == -1) {
+					System.err.println("column name is not match. xColumn = " + xColumn + ", yColumn = " + yColumn + ", valueColumn = " + valueColumn + ". @" + filename);
+					continue;
 				}
 
 				for (int j = 1; j < rowNum; j++) {
@@ -137,7 +263,7 @@ public class EntryApp implements UncaughtExceptionHandler {// an application
 						ElementProperty eTemp = element[k];
 						if ((x >= eTemp.xMin) && (x <= eTemp.xMax)
 								&& (y >= eTemp.yMin) && (y <= eTemp.yMax)) {
-							value[k][i - 1] = sheet.getCell(valueColumn, j)
+							value[k][i] = sheet.getCell(valueColumn, j)
 									.getContents();
 						}
 					}
@@ -146,14 +272,15 @@ public class EntryApp implements UncaughtExceptionHandler {// an application
 			} catch (Exception e) {
 				e.printStackTrace();
 				System.out.println(e);
+				System.exit(-3);
 			}
 		}
 
 		// sample data write to new excel file
-		System.out.println("Writing result to " + fileName[1]);
+		System.out.println("Writing result to " + app.getResultFilename());
 		try {
 			WritableWorkbook book = Workbook.createWorkbook(new File(
-					fileName[1]));
+					app.getResultFilename()));
 			WritableSheet wrtSheet = book.createSheet("the first page", 0);
 
 			for (int i = 0; i < element.length; i++) {
@@ -180,7 +307,7 @@ public class EntryApp implements UncaughtExceptionHandler {// an application
 		}
 	}
 
-	@Override
+	// @Override
 	public void uncaughtException(Thread t, Throwable info) {
 		info.printStackTrace();
 	}
